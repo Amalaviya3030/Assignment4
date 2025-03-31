0bc5673427328d881fa98a847f8022b99d4bb991
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-03-31 14:51:55 -0400

    create the struct for the chatmessage and set the port and buffersize and call the function name is handle_error

--- chat_protocol.h
+++ chat_protocol.h
@@ -12,3 +12,13 @@
 #define MSG_TYPE_CHAT 1
 #define MSG_TYPE_JOIN 2
 #define MSG_TYPE_LEAVE 3
+
+
+// protocols
+typedef struct {
+    uint8_t type;
+    char username[MAX_USERNAME];
+    char message[MAX_MESSAGE];
+} ChatMessage;
+
+#endif 
