0ce7424780761a43c1563dd2e793483d7d07026c
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-03-31 13:37:32 -0400

    create the chat protocol leader dor the basic message types

--- chat_protocol.h
+++ chat_protocol.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef CHAT_PROTOCOL_H
+#define CHAT_PROTOCOL_H
+
+#include <stdint.h>
+
+#define PORT 8080
+#define MAX_LINES 100
+#define MAX_USERNAME 50
+#define MAX_MESSAGE 1024
+
+// messages
+#define MSG_TYPE_CHAT 1
+#define MSG_TYPE_JOIN 2
+#define MSG_TYPE_LEAVE 3
