82925ffb1537ccb788749e074ea8cb21cddb2fdf
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-03-31 21:12:45 -0400

    messages are stored with timestamp and chat history is also managed here

--- client_utils.c
+++ client_utils.c
@@ -12,5 +12,22 @@ extern char chat_history[MAX_LINES][BUFFER_SIZE];
 extern int line_count;
 
 void update_chat_window(const char* message) {
+    time_t now;
+    time(&now);
+    char timestamp[26];
+    ctime_r(&now, timestamp);
+    timestamp[24] = '\0';
+
+    if (line_count < MAX_LINES) {
+        snprintf(chat_history[line_count], BUFFER_SIZE, "[%s] %s", timestamp, message);
+        line_count++;
+    }
+    else {
+        for (int i = 1; i < MAX_LINES; i++) {
+            strcpy(chat_history[i - 1], chat_history[i]);
+        }
+        snprintf(chat_history[MAX_LINES - 1], BUFFER_SIZE, "[%s] %s", timestamp, message);
+    }
+
 
 }
